cd3099197a38649e300a921f40657a84ede7a73c
nezassal <sergey.mindar@gmail.com>
2024-01-05 10:31:37 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-import './style.css';
-import Map from './src/ol/Map.js';
-import OSM from './src/ol/source/OSM.js';
-import TileLayer from './src/ol/layer/Tile.js';
-import View from './src/ol/View.js';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
 
 const map = new Map({
-  target: 'map',
   layers: [
     new TileLayer({
       source: new OSM(),
     }),
   ],
+  target: 'map',
   view: new View({
     center: [0, 0],
     zoom: 2,
   }),
 });
+
+document.getElementById('zoom-out').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom - 1);
+};
+
+document.getElementById('zoom-in').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom + 1);
+};
