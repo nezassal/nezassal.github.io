0759f79c51e25f66ee15b9dac3bacb2b21fb97f2
nezassal <sergey.mindar@gmail.com>
2024-01-08 14:47:41 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
+/*
 import Map from './src/ol/Map.js';
 import OSM from './src/ol/source/OSM.js';
 import TileLayer from './src/ol/layer/Tile.js';
 import View from './src/ol/View.js';
+*/
 
-const map = new Map({
+const map = new ol.Map({
   layers: [
-    new TileLayer({
-      source: new OSM(),
+    new ol.TileLayer({
+      source: new ol.OSM(),
     }),
   ],
   target: 'map',
-  view: new View({
+  view: new ol.View({
     center: [0, 0],
     zoom: 2,
   }),
